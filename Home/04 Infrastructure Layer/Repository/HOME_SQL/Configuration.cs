0a42ee611c92568269f10b7e891ca3c4f8013c8f
Dason <Dason@DASON-PC>
2017-02-24 14:59:19 +0800

    修正 實體腳本 生成默認值。讓其它程序能對表進行操作，不必處理實體中與業務邏輯無關的必填值

--- Home/04 Infrastructure Layer/Repository/HOME_SQL/Configuration.cs
+++ Home/04 Infrastructure Layer/Repository/HOME_SQL/Configuration.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Migrations.Model;
+using System.Data.Entity.SqlServer;
+
 namespace Home.Repository.HOME_SQL
 {
     using global::Repository;
@@ -10,15 +15,16 @@ namespace Home.Repository.HOME_SQL
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
             MigrationsDirectory = @"HOME_SQL";
+            SetSqlGenerator("System.Data.SqlClient", new CustomSqlServerMigrationSqlGenerator());
         }
 
         protected override void Seed(MainBoundedContext context)
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data. E.g.
             //
             //    context.People.AddOrUpdate(
@@ -28,6 +34,54 @@ namespace Home.Repository.HOME_SQL
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+        }
+    }
+
+    internal class CustomSqlServerMigrationSqlGenerator : SqlServerMigrationSqlGenerator
+    {
+        protected override void Generate(AddColumnOperation addColumnOperation)
+        {
+            SetCreatedUtcColumn(addColumnOperation.Column);
+
+            base.Generate(addColumnOperation);
+        }
+
+        protected override void Generate(CreateTableOperation createTableOperation)
+        {
+            SetCreatedUtcColumn(createTableOperation.Columns);
+
+            base.Generate(createTableOperation);
+        }
+
+        private static void SetCreatedUtcColumn(IEnumerable<ColumnModel> columns)
+        {
+            foreach (var columnModel in columns)
+            {
+                SetCreatedUtcColumn(columnModel);
+            }
+        }
+
+        private static readonly string[] TimeColumnNames = { "Modified", "Created" };
+        private static readonly string[] UserColumnNames = { "ModifiedBy", "CreatedBy" };
+
+        private static void SetCreatedUtcColumn(ColumnModel column)
+        {
+            if (column.IsNullable == false && column.Type == PrimitiveTypeKind.DateTime)
+            {
+                if (TimeColumnNames.Any(n => string.Equals(n, column.Name, StringComparison.OrdinalIgnoreCase)))
+                    column.DefaultValueSql = "getdate()";
+            }
+            if (UserColumnNames.Any(n => string.Equals(n, column.Name, StringComparison.OrdinalIgnoreCase)))
+                column.DefaultValueSql = "'script'";
+            if (column.IsNullable == false && string.Equals("Id", column.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                if (column.Type == PrimitiveTypeKind.Guid) column.DefaultValueSql = "newid()";
+                if (column.Type == PrimitiveTypeKind.Int32) column.IsIdentity = true;
+            }
+            if (string.Equals("StatusCode", column.Name, StringComparison.OrdinalIgnoreCase) && column.Type == PrimitiveTypeKind.Int32)
+            {
+                column.DefaultValueSql = "2";
+            }
         }
     }
 }
--- Home/04 Infrastructure Layer/Repository/Migrations/Configuration.cs
+++ Home/04 Infrastructure Layer/Repository/Migrations/Configuration.cs
@@ -1,16 +1,24 @@
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Migrations.Model;
+using System.Data.Entity.Migrations.Sql;
+using MySql.Data.Entity;
+
 namespace Repository.Migrations
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-   // [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+
+    // [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
     internal sealed class Configuration : DbMigrationsConfiguration<Repository.MainBoundedContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
             MigrationsDirectory = @"Migrations";
+            SetSqlGenerator("MySql.Data.MySqlClient", new CustomMySqlMigrationSqlGenerator());
         }
 
         protected override void Seed(Repository.MainBoundedContext context)
@@ -27,6 +35,54 @@ namespace Repository.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+        }
+    }
+
+    internal class CustomMySqlMigrationSqlGenerator : MySqlMigrationSqlGenerator
+    {
+        protected override MigrationStatement Generate(AddColumnOperation addColumnOperation)
+        {
+            SetCreatedUtcColumn(addColumnOperation.Column);
+
+            return base.Generate(addColumnOperation);
+        }
+
+        protected override MigrationStatement Generate(CreateTableOperation createTableOperation)
+        {
+            SetCreatedUtcColumn(createTableOperation.Columns);
+
+            return base.Generate(createTableOperation);
+        }
+
+        private void SetCreatedUtcColumn(IEnumerable<ColumnModel> columns)
+        {
+            foreach (var columnModel in columns)
+            {
+                SetCreatedUtcColumn(columnModel);
+            }
+        }
+
+        private static readonly string[] TimeColumnNames = { "Modified", "Created" };
+        private static readonly string[] UserColumnNames = { "ModifiedBy", "CreatedBy" };
+
+        private void SetCreatedUtcColumn(ColumnModel column)
+        {
+            if (column.IsNullable == false && column.Type == PrimitiveTypeKind.DateTime)
+            {
+                if (TimeColumnNames.Any(n => string.Equals(n, column.Name, StringComparison.OrdinalIgnoreCase)))
+                    column.DefaultValueSql = "now()";
+            }
+            if (UserColumnNames.Any(n => string.Equals(n, column.Name, StringComparison.OrdinalIgnoreCase)))
+                column.DefaultValueSql = "'script'";
+            if (column.IsNullable == false && string.Equals("Id", column.Name, StringComparison.OrdinalIgnoreCase))
+            {
+                if (column.Type == PrimitiveTypeKind.Guid) column.DefaultValueSql = "uuid()";
+                if (column.Type == PrimitiveTypeKind.Int32) column.IsIdentity = true;
+            }
+            if (string.Equals("StatusCode", column.Name, StringComparison.OrdinalIgnoreCase) && column.Type == PrimitiveTypeKind.Int32)
+            {
+                column.DefaultValueSql = "2";
+            }
         }
     }
 }
