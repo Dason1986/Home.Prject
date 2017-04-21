5a5aa3ca7ad977048d8e0d71dce06bfe89f21287
Dason <Dason@DASON-PC>
2017-04-21 22:50:43 +0800

    拆解

---
+++ Home/02 Applicatioin Layer/HomeApplication/ComponentModel/IO/PhotoBuilder.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using Library.Draw;
+using Library.HelperUtility;
+
+namespace HomeApplication.ComponentModel.IO
+{
+    public abstract class PhotoBuilder
+    {
+        private Image _sourceImage;
+        public Orientation Orientation { get; set; }
+        private Rectangle _sourceRectangle;
+
+        public Image SourceImage
+        {
+            get { return _sourceImage; }
+            set
+            {
+                _sourceImage = value;
+                _sourceRectangle = new Rectangle(0, 0, SourceImage.Width, SourceImage.Height);
+            }
+        }
+
+        protected Rectangle SourceRectangle
+        {
+            get { return _sourceRectangle; }
+        }
+
+        protected void Rotate(Bitmap tempImage)
+        {
+            switch (Orientation)
+            {
+                case Library.Draw.Orientation.Rotate180:
+                    tempImage.RotateFlip(RotateFlipType.Rotate180FlipX);
+                    break;
+
+                case Library.Draw.Orientation.Rotate270CW:
+                    tempImage.RotateFlip(RotateFlipType.Rotate270FlipX);
+                    break;
+
+                case Library.Draw.Orientation.Rotate90CW:
+                    tempImage.RotateFlip(RotateFlipType.Rotate90FlipX);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public abstract Stream Create(Image image);
+    }
+    public class PhotoPanoramicBuilder : PhotoBuilder
+    {
+
+
+         
+
+        public override Stream Create(Image image)
+        {
+            const int fixHeight = 1080;
+            var tempheight = image.Size.Height > fixHeight ? fixHeight : image.Size.Height;
+            var per = (decimal)tempheight / image.Height;
+            var tempWidth = (int)(image.Width * per);
+            Bitmap tempImage = new Bitmap(tempWidth, tempheight, PixelFormat.Format64bppArgb);
+
+            Graphics g = Graphics.FromImage(tempImage);
+            g.DrawImage(image, new RectangleF(0, 0, tempWidth, tempheight), SourceRectangle, GraphicsUnit.Pixel);
+            g.Dispose();
+            MemoryStream ms = new MemoryStream();
+            tempImage.Save(ms, ImageFormat.Jpeg);
+
+            tempImage.Dispose();
+            return ms;
+        }
+
+
+
+
+
+
+    }
+    public class PhotoThumbnailBuilder : PhotoBuilder
+    {
+        public PhotoThumbnailBuilder()
+        {
+            ThumbnailHasBorder = true;
+        }
+
+
+        public bool ThumbnailHasBorder { get; set; }
+
+
+ 
+
+
+        private const int MamarginLr = 10;
+        private const int MamarginTb = 230;
+        private static readonly RectangleF ThumbnailFillRectangle = new RectangleF(MamarginLr, MamarginLr, MamarginTb, MamarginTb);
+        private const int ThumbnailPixels = 234;
+
+
+
+        public override Stream Create(Image image)
+        {
+            var min = Math.Min(image.Width, image.Height);
+            var max = Math.Max(image.Width, image.Height);
+            Bitmap tempImage = new Bitmap(250, 250, PixelFormat.Format64bppArgb);
+
+            Graphics g = Graphics.FromImage(tempImage);
+            g.Clear(Color.White);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            if (ThumbnailHasBorder)
+            {
+                g.DrawRectangle(new Pen(Color.Gray, 4), 8, 8, 234, 234);
+                g.DrawRectangle(new Pen(Color.LightGray, 2), 2, 2, 246, 246);
+            }
+            if (min < 250)
+            {
+                if (min == image.Width)
+                {
+                    if (max > ThumbnailPixels)
+                    {
+                        g.DrawImage(image, ThumbnailFillRectangle, SourceRectangle, GraphicsUnit.Pixel);
+                    }
+                    else
+                    {
+                        var per = (decimal)ThumbnailPixels / image.Height;
+                        var tempWidth = (int)(image.Width * per);
+                        var paddingleft = (ThumbnailPixels - tempWidth) / 2;
+                        g.DrawImage(image, new Rectangle(paddingleft, 12, tempWidth, 226), SourceRectangle, GraphicsUnit.Pixel);
+                    }
+                }
+                else
+                {
+                    if (min > ThumbnailPixels)
+                    {
+                        g.DrawImage(image, ThumbnailFillRectangle, SourceRectangle, GraphicsUnit.Pixel);
+                    }
+                    else
+                    {
+                        var per = (decimal)ThumbnailPixels / image.Width;
+
+                        var tempHeight = (int)(image.Height * per);
+                        var paddingTop = (ThumbnailPixels - tempHeight) / 2;
+                        g.DrawImage(image, new Rectangle(12, paddingTop, 226, tempHeight), SourceRectangle, GraphicsUnit.Pixel);
+                    }
+                }
+            }
+            else
+            {
+                g.DrawImage(image, ThumbnailFillRectangle, SizeUtility.GetSquareRectangle(image.Size), GraphicsUnit.Pixel);
+            }
+
+            g.Dispose();
+
+            Rotate(tempImage);
+            MemoryStream ms = new MemoryStream();
+            tempImage.Save(ms, ImageFormat.Jpeg);
+
+            tempImage.Dispose();
+            return ms;
+        }
+
+
+    }
+    public class PhotoZoomBuilder : PhotoBuilder
+    {
+
+
+
+
+        public Size ZoomSize { get; set; }
+
+ 
+
+
+
+        public override Stream Create(Image image)
+        {
+            Size endPoint = image.Size;
+            if ((image.Width < ZoomSize.Width && image.Height < ZoomSize.Height) == false)
+            {
+                endPoint = SizeUtility.ZoomSizeByPixels(endPoint, image.Width > image.Height ? ZoomSize.Height : ZoomSize.Width);
+            }
+
+            Bitmap tempImage = new Bitmap(endPoint.Width, endPoint.Height, PixelFormat.Format64bppArgb);
+
+            Graphics g = Graphics.FromImage(tempImage);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(image, new Rectangle(Point.Empty, endPoint), SourceRectangle, GraphicsUnit.Pixel);
+
+            g.Dispose();
+            Rotate(tempImage);
+            MemoryStream ms = new MemoryStream();
+            tempImage.Save(ms, ImageFormat.Jpeg);
+
+            tempImage.Dispose();
+            return ms;
+        }
+
+
+
+
+    }
+}
---
+++ Home/02 Applicatioin Layer/HomeApplication/Logic/IO/ScanderFTP.cs
@@ -0,0 +1,132 @@
+﻿using FileEx = Home.DomainModel.Aggregates.FileAgg.FileInfo;
+using Home.DomainModel.ModuleProviders;
+using Library.ComponentModel.Logic;
+using Library.Infrastructure.Application;
+using Library;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+using Home.DomainModel.Aggregates.FileAgg;
+using Home.DomainModel.DomainServices;
+using Home.DomainModel.Repositories;
+using HomeApplication.DomainServices;
+using Library.Storage.FileEngineProvider.Network;
+
+namespace HomeApplication.Logic.IO
+{
+    public struct ScanderFTPOption : IOption
+    {
+        public string Path { get; set; }
+
+        public string Server { get; set; }
+
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class ScanderFTP : BaseMultiThreadingLogicService
+    {
+        public ScanderFTPOption Option { get; set; }
+
+        protected override IOption ServiceOption
+        {
+            get
+            {
+                return Option;
+            }
+
+            set
+            {
+                Option = (ScanderFTPOption)value;
+            }
+        }
+
+        private string _path;
+        Limilabs.FTP.Client.Ftp ftp = new Limilabs.FTP.Client.Ftp();
+        protected override bool OnVerification()
+        {
+            if (string.IsNullOrEmpty(Option.Path)) throw new Exception("路徑爲空");
+
+            try
+            {
+
+                ftp.Connect(Option.Server);
+                ftp.Login(Option.User, Option.Password);
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("連接失敗", e);
+            }
+            //_path = Path.GetFullPath(Option.Path);
+            //if (_path[0] == '\'' || _path[0] == '"') _path = _path.Substring(1, Option.Path.Length - 2);
+            //if (!Directory.Exists(_path)) Directory.CreateDirectory(_path);
+            //var storageEngineRepository = Bootstrap.Currnet.GetService<IStorageEngineRepository>();
+            //_engin = storageEngineRepository.GetByPathEngine(_path);
+            //if (_engin == null)
+            //{
+            //    _engin = new Home.DomainModel.Aggregates.FileAgg.StorageEngine(CreatedInfo.ScanderPhysical)
+            //    {
+            //        Root = _path,
+            //        Name = Path.GetDirectoryName(_path)
+            //    };
+            //    storageEngineRepository.Add(_engin);
+            //    storageEngineRepository.UnitOfWork.Commit();
+            //}
+            Scan(_path);
+            return base.OnVerification();
+        }
+
+        protected override void ThreadProssSize(int beginindex, int endindex, int take)
+        {
+            try
+            {
+                var provider = Bootstrap.Currnet.GetService<IFileManagentModuleProvider>();
+
+                var files = _files.Skip(beginindex).Take(take).Select(n => new System.IO.FileInfo(n)).ToArray();
+
+                #region MyRegion
+
+                var domainService = Bootstrap.Currnet.GetService<IAddFileDomainService>();
+                domainService.ModuleProvider = provider;
+
+                domainService.Handle(new AddFileEventArgs(_engin, files, Home.DomainModel.SourceType.ServerScand, CreatedInfo.PhotoFileAnalysis));
+                domainService.ModuleProvider.UnitOfWork.Commit();
+
+                GC.Collect();
+
+                #endregion MyRegion
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        private readonly IList<string> _files = new List<string>();
+        private StorageEngine _engin;
+
+        private void Scan(string dic)
+        {
+            var list = ftp.GetList(dic);
+
+            foreach (var ftpItem in list)
+            {
+                if (ftpItem.IsFile)
+                    this._files.Add(ftpItem.SymlinkPath);
+                if (ftpItem.IsFolder)
+                    Scan(ftpItem.SymlinkPath);
+            }
+
+
+
+        }
+
+        protected override int GetTotalRecord()
+        {
+            return _files.Count;
+        }
+    }
+}
